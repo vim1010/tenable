f7afd3f09ea31b0d0c87e4e59c1da35858f15540
vim1010 <cloudops@sent.com>
2023-06-02 15:53:02 -0400

    app

---
+++ etl.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/env perl
+
+use LWP::UserAgent;
+use Getopt::Long;
+use JSON::PP;
+use Carp;
+use HTTP::Request;
+use URI;
+use v5.32;
+
+my $ua = LWP::UserAgent->new();
+my $tsc_base_url = $ENV{TSC_BASE_URL};
+my $tsc_key = $ENV{TSC_KEY};
+my $tsc_secret = $ENV{TSC_SECRET};
+
+sub log_and_croak {
+  my ($code, $res) = @_;
+  my $msg = encode_json($res);
+  croak "$code: $msg";
+}
+
+sub http_request {
+  my ($method, $route, $d) = @_;
+  my $url = URI->new("$tsc_base_url/rest/$route");
+  if ($d->{query}) {
+    $url->query_form(%{$d->{query}});
+  }
+  my $req = HTTP::Request->new($method, $url);
+  $req->header('Content-Type' => 'application/json');
+  $req->header('x-apikey' => "accesskey=$tsc_key; secretkey=$tsc_secret;");
+  if (defined $d->{data}) {
+    my $jsn = encode_json($d->{data});
+    $req->content($jsn);
+  }
+  my $res = $ua->request($req);
+  my $t = $res->content;
+  my $h = decode_json($t);
+  return ($res->code, $h);
+}
+
+sub get_scans {
+  my ($code, $d) = http_request('GET', '/scan');
+  say encode_json($d);
+}
+
+sub get_scan_result {
+  my $id = shift;
+  my ($code, $d) = http_request('GET', "/scanResult/$id");
+  say encode_json($d);
+} 
+
+sub main {
+  my $arg = shift @ARGV;
+  GetOptions(
+    'id=i' => \my $id
+  );
+  if ($arg =~ /^scans$/) {
+    get_scans;
+  } elsif ($arg =~ /^scan$/) { 
+    get_scan_result($id);
+  } else {
+    say "bad arg: $arg";
+  }
+}
+
+main;
