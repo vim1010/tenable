3475eafdea55a0ba73a5eef2c17d4fa341d4278d
vim1010 <cloudops@sent.com>
2023-06-05 15:23:31 -0400

    app

--- gotsc/main.go
+++ gotsc/main.go
@@ -6,15 +6,42 @@ import (
 )
 
 var model *RexService
+var tsc *TscService
 
 func main() {
-	baseURL := os.Getenv("REX_BASE_URL")
-	user := os.Getenv("REX_USER")
-	pass := os.Getenv("REX_PASS")
+	rexURL := os.Getenv("REX_BASE_URL")
+	rexUser := os.Getenv("REX_USER")
+	rexPass := os.Getenv("REX_PASS")
+	tscURL := os.Getenv("TSC_URL")
+	tscKey := os.Getenv("TSC_KEY")
+	tscSecret := os.Getenv("TSC_SECRET")
 	model = NewRexService(
-		baseURL,
-		user,
-		pass,
+		rexURL,
+		rexUser,
+		rexPass,
 	)
-	fmt.Println(model)
+	if model == nil {
+		panic("model error")
+	}
+	tsc = NewTscService(
+		tscURL,
+		tscKey,
+		tscSecret,
+	)
+	opts := map[string]any{
+		"query": map[string]any{
+			"tool":        "vulnipsummary",
+			"endOffset":   1000,
+			"startOffset": 0,
+			"type":        "vuln",
+			"filters":     []any{},
+		},
+		"type":       "vuln",
+		"sourceType": "patched",
+	}
+	d, err := tsc.Post("/analysis", opts, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(d))
 }
